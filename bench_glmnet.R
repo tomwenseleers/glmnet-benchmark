e9b497504211dc8e26dcb0bd57d30ed0649268b5
Tom Wenseleers <12063422+tomwenseleers@users.noreply.github.com>
2023-10-11 20:23:35 +0200

    added sessionInfo()

--- bench_glmnet.R
+++ bench_glmnet.R
@@ -222,3 +222,27 @@ system.time(L0glm_dense_bigglm <- L0glm.fit(X_dense, y,
 R = cor(as.vector(L0glm_dense_bigglm$coefficients), coefs_true)
 print("Correlation between estimated & true coefs:\n")
 print(round(R, 4)) # 0.9399 - much better solution than LASSO
+
+
+sessionInfo()
+# R version 4.3.1 (2023-06-16 ucrt)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 11 x64 (build 22621)
+# 
+# Matrix products: default
+# 
+# 
+# locale:
+# [1] LC_COLLATE=English_United States.utf8  LC_CTYPE=English_United States.utf8   
+# [3] LC_MONETARY=English_United States.utf8 LC_NUMERIC=C                          
+# [5] LC_TIME=English_United States.utf8    
+# 
+# time zone: Europe/Paris
+# tzcode source: internal
+# 
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+# [1] export_0.3.0          abess_0.4.8           dplyr_1.1.3           L0glm_0.2.0          
+# [5] microbenchmark_1.4.10 glmnet_4.1-8          Matrix_1.6-1.1  
